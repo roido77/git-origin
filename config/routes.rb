63add7844b3e94a183f68e848f8d29eca9d1b4c0
EC2 Default User <ec2-user@ip-172-31-28-142.ec2.internal>
2022-10-22 13:23:52 +0000

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'seets/index'
-  get 'seets/create'
-  get 'seets/show'
-  get 'seets/edit'
-  get 'seets/new'
-  get 'seets/update'
    devise_for :users
+   resources :seets, only: [ :create, :index, :show, :new,:edit,:update,]
    root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
