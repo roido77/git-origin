a707470bb2f809696d7d0d5b74ef990ac9ff4fcc
EC2 Default User <ec2-user@ip-172-31-28-142.ec2.internal>
2022-10-23 03:29:00 +0000

    新規投稿するのに必要なアクションとストロングパラメーターを記入した

--- app/controllers/seets_controller.rb
+++ app/controllers/seets_controller.rb
@@ -3,6 +3,9 @@ class SeetsController < ApplicationController
   end
 
   def create
+    seet = Seet.new(seet_params)
+    seet.save
+    redirect_to seets_path
   end
 
   def show
@@ -12,9 +15,15 @@ class SeetsController < ApplicationController
   end
 
   def new
-    
+    @seet = Seet.new
   end
 
   def update
+  end
+  
+  private
+  
+  def seet_params
+    params.require(:seet).permit(:shop_name, :image, :caption, :rinku )
   end
 end
