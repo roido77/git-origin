37df892cb64e73b3b24e4ddf4424b1dcea17fec7
EC2 Default User <ec2-user@ip-172-31-28-142.ec2.internal>
2022-10-29 13:03:29 +0000

    詳細ページを投稿真ん中ぞろえにしてヘッダーの上を空白にした

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
    devise_for :users
    resources :seets, only: [ :create, :index, :show, :new,:edit,:update,]
+   
    root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
